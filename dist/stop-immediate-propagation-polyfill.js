08b0ec2903a375c2cbab3e23750bac3fbf4ac94c
afeiship <1290657123@qq.com>
2018-01-02 21:20:31 +0800

    optimize

--- dist/stop-immediate-propagation-polyfill.js
+++ dist/stop-immediate-propagation-polyfill.js
@@ -1,28 +1,31 @@
 if (!Event.prototype.stopImmediatePropagation) {
-	(function() {
-		'use strict';
-		var addEventListener = Node.prototype.addEventListener, removeEventListener = Node.prototype.removeEventListener;
+  (function () {
+    'use strict';
+    var addEventListener = Node.prototype.addEventListener;
+    var removeEventListener = Node.prototype.removeEventListener;
 
-		Node.prototype.addEventListener = function(type, callback, capture) {
-			addEventListener.call(this, type, callback.hijackedCallback || (callback.hijackedCallback = function(event) {
-				if (!event.immediatePropagationStopped) {
-					callback(event);
-				}
-			}), capture);
-		};
+    Node.prototype.addEventListener = function (type, callback, capture) {
+      callback.hijackedCallback = function (event) {
+        if (!event.immediatePropagationStopped) {
+          callback(event);
+        }
+      };
+      addEventListener.call(this, type, callback.hijackedCallback, capture);
+    };
 
-		Node.prototype.removeEventListener = function(type, callback, capture) {
-			removeEventListener.call(this, type, callback.hijackedCallback || callback, capture);
-		};
-	}());
+    Node.prototype.removeEventListener = function (type, callback, capture) {
+      removeEventListener.call(this, type, callback.hijackedCallback || callback, capture);
+    };
+  }());
 
-	Event.prototype.stopImmediatePropagation = function() {
-		'use strict';
-		this.immediatePropagationStopped = true;
-		this.stopPropagation();
+  Event.prototype.stopImmediatePropagation = function () {
+    'use strict';
+    this.immediatePropagationStopped = true;
+    this.stopPropagation();
   };
 
-  Event.prototype.isImmediatePropagationStopped = function(){
+  Event.prototype.isImmediatePropagationStopped = function () {
+    'use strict';
     return this.immediatePropagationStopped;
   };
 }
